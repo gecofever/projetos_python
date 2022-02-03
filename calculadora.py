6141339068113b3fe924e21ff7b3915943d6043d
Germano B. Nascimento <96051851+gecofever@users.noreply.github.com>
2022-02-03 10:27:16 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+a = float(input ("Digite o primeiro numero:"))
+b = float(input ("Digite o segundo numero:"))
+operacao = input ("Qual a operação ?")
+if operacao == "+":
+    print (a+b)
+elif operacao == "-":
+    print (a-b)
+elif operacao == "*":
+    print (a*b)
+else:
+    operacao == "/"
+    print (a/b)
