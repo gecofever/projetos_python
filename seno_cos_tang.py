357c6fb429145c277cc155da0fa79f3cbbd9b687
Germano B. Nascimento <96051851+gecofever@users.noreply.github.com>
2023-07-14 15:19:29 -0300

    Delete seno_cos_tang.py

--- seno_cos_tang.py
+++
@@ -1,8 +0,0 @@
-import math
-angulo = int (input ("Digite o Angulo que você deseja: "))
-seno = math.sin(math.radians(angulo))
-cosseno = math.cos(math.radians(angulo))
-tangente = math.tan(math.radians(angulo))
-print("O valor do Sen é:", seno)
-print("O valor do Cos é:", cosseno)
-print("O valor da Tan é:", tangente)
